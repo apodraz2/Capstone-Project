ea719350f2df796a0ca202d36b04bd00ce3bcbe3
Adam Podraza <apodra86@gmail.com>
2016-03-02 11:02:14 -0700

    finished datacontract and dbhelper

--- GaoGao/app/src/main/java/com/podraza/android/gaogao/gaogao/content_provider/DataContract.java
+++ GaoGao/app/src/main/java/com/podraza/android/gaogao/gaogao/content_provider/DataContract.java
@@ -15,6 +15,10 @@ public class DataContract {
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
 
     public static final String PATH_USER = "user";
+    public static final String PATH_DOG = "dog";
+    public static final String PATH_TODO = "todo";
+    public static final String PATH_USER_DOG = "user_dog";
+    public static final String PATH_DOG_TODO = "dog_todo";
 
     public static final class UserEntry implements BaseColumns {
 
@@ -28,11 +32,86 @@ public class DataContract {
                 ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_USER;
 
         public static final String TABLE_NAME = "user";
+        public static final String COLUMN_ID = "id";
         public static final String COLUMN_NAME = "name";
         public static final String COLUMN_EMAIL = "email";
 
 
         public static final String COLUMN_USER_SETTING = "user_setting";
+
+        public static Uri buildDataUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+    }
+
+    public static final class DogEntry implements BaseColumns {
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_DOG).build();
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_DOG;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_DOG;
+
+        public static final String TABLE_NAME = "dog";
+        public static final String COLUMN_NAME = "name";
+        public static final String COLUMN_ID = "id";
+
+        public static Uri buildDataUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+
+    }
+
+    public static final class TodoEntry implements BaseColumns {
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_TODO).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_TODO;
+
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_TODO;
+
+        public static final String TABLE_NAME = "todo";
+        public static final String COLUMN_DESCRIPTION = "description";
+        public static final String COLUMN_ID = "id";
+
+        public static Uri buildDataUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+    }
+
+    public static final class UserDog implements BaseColumns {
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_USER_DOG).build();
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_USER_DOG;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_USER_DOG;
+
+        public static final String TABLE_NAME = "user_dog";
+        public static final String COLUMN_DOG = "dog";
+        public static final String COLUMN_USER = "user";
+        public static final String COLUMN_ID = "id";
+
+        public static Uri buildDataUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+    }
+
+    public static final class DogTodo implements BaseColumns {
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_DOG_TODO).build();
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_DOG_TODO;
+
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_DOG_TODO;
+
+        public static final String TABLE_NAME = "user_todo";
+        public static final String COLUMN_DOG = "dog";
+        public static final String COLUMN_TODO = "todo";
+        public static final String COLUMN_ID = "id";
 
         public static Uri buildDataUri(long id) {
             return ContentUris.withAppendedId(CONTENT_URI, id);
